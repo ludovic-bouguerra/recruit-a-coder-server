d6997103118631f8888c273aefc80fd93b2108ba
Ludovic Bouguerra <bouguerra.ludovic@gmail.com>
2013-07-27 00:08:27 +0200

    Correction bug compilation

---
+++ src/main/java/fr/ludovicbouguerra/ecodigo/messageprotocol/ParametersException.java
@@ -0,0 +1,10 @@
+package fr.ludovicbouguerra.ecodigo.messageprotocol;
+
+public class ParametersException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8403508996758078011L;
+
+}
